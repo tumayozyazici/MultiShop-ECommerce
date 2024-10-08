214ef7fc67f0b7754eadf0c88d03f6d42a7732f4
tumay ozyazici <tumay.mm@gmail.com>
2024-10-09 01:54:09 +0300

    UI Kısmı genel olarak oluşturuldu. Admin kısmına geçiyoruz.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductListViewComponents/_GetProductCountProductListComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _GetProductCountProductListComponentPartial  :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
